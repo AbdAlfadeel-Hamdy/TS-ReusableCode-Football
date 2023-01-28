562018ffcdaaa6e99c133bff973f2d72b5ea82c8
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-01-28 03:23:01 +0200

    HtmlReport class

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,21 @@ import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 import { Summary } from "./Summary";
-// const reader = new MatchReader("3.1 football.csv.csv");
-// reader.read();
 
 const csvFileReader = new CsvFileReader("3.1 football.csv.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Liverpool"), new ConsoleReport());
-summary.buildAndPrintReport(matchReader.matches);
+const summaryLiverpool = new Summary(
+  new WinsAnalysis("Liverpool"),
+  new ConsoleReport()
+);
+summaryLiverpool.buildAndPrintReport(matchReader.matches);
+
+const summaryChelsea = new Summary(
+  new WinsAnalysis("Chelsea"),
+  new HtmlReport("chelseaReport")
+);
+summaryChelsea.buildAndPrintReport(matchReader.matches);
