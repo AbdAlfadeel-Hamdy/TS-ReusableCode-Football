4d3da6a43160b813cdedfa17ac3747867225ff3d
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-01-28 02:45:01 +0200

    WinsAnalysis Class

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,3 @@ import { MatchResult } from "./MatchResult";
 const csvFileReader = new CsvFileReader("3.1 football.csv.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-let unitedWins = 0;
-for (const match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin)
-    unitedWins++;
-  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
-    unitedWins++;
-}
-
-console.log(`Man United won ${unitedWins} games`);
